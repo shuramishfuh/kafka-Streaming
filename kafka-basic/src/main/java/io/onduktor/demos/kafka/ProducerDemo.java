d03d36595a0acca6933805de94a78d6d6a2d1382
ShuramishFuh <agamatimtim@gmail.com>
2022-03-17 14:41:36 -0500

    wrote producer without callback

--- kafka-basic/src/main/java/io/onduktor/demos/kafka/ProducerDemo.java
+++ kafka-basic/src/main/java/io/onduktor/demos/kafka/ProducerDemo.java
@@ -1,7 +1,41 @@
 package io.onduktor.demos.kafka;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
 public class ProducerDemo {
+
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName()); // logger from this class
     public static void main(String[] args) {
-        System.out.println("cool");
+        log.info("cool");
+
+
+        // create producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        // create the producer
+        KafkaProducer<String, String> producer =new KafkaProducer<String, String>(properties);
+
+        // create a producer record
+        ProducerRecord<String,String> producerRecord = new ProducerRecord<>("demo_java","i am a kafka producer");
+
+        //send data -async
+        producer.send(producerRecord);
+
+        // flush  -sync
+        producer.flush();
+
+        // flush and close
+        producer.close();
+
     }
 }
