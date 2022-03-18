a14483c02be82c6cbcacbc8262a4446906120d59
ShuramishFuh <agamatimtim@gmail.com>
2022-03-17 19:25:13 -0500

    wrote consumer with cooperation

--- kafka-basic/src/main/java/io/onduktor/demos/kafka/ConsumerDemoWithCooperative.java
+++ kafka-basic/src/main/java/io/onduktor/demos/kafka/ConsumerDemoWithCooperative.java
@@ -1,9 +1,6 @@
 package io.onduktor.demos.kafka;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -13,9 +10,9 @@ import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
 
-public class ConsumerDemoWithShutdown {
+public class ConsumerDemoWithCooperative {
 
-    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithShutdown.class.getSimpleName()); // logger from this class
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoWithCooperative.class.getSimpleName()); // logger from this class
     public static void main(String[] args) {
         log.info("I am a kafka consumer");
         String bootstrapsServer = "127.0.0.1:9092";
@@ -30,6 +27,7 @@ public class ConsumerDemoWithShutdown {
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        properties.setProperty(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, CooperativeStickyAssignor.class.getName());
 
         // create consumer
         KafkaConsumer<String,String> consumer = new KafkaConsumer<>(properties);
